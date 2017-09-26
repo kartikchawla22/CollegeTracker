d565d3be608e19a4015fe6de3cfe5f323ab3588e
Jaspreet Singh <jaspreet.lamba@kelltontech.com>
2017-09-26 15:20:47 +0530

    initial

---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'profile',
+  styleUrls: ['./profile.scss'],
+  templateUrl: './profile.html'
+})
+export class ProfileComponent {
+  constructor() {}
+}
